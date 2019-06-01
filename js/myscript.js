c7b7d06d959641a426b96764291fccb0f2c6a73b
ma-ya's create <hello.myscreate@gmail.com>
2019-06-02 01:54:32 +0900

    update: changed interaction of spNav pattern

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,8 @@
 ////////////scrollEvents_start////////////
-jQuery(function(){
+jQuery(function () {
 
   //navHeaderShow_variables
-  var cloneNav = jQuery('#navHeader').clone().attr('id','cloneNav').addClass('cloneNav').insertAfter('#navHeader');
+  var cloneNav = jQuery('#navHeader').clone().attr('id', 'cloneNav').addClass('cloneNav').insertAfter('#navHeader');
   var logoMove = jQuery('.logoFollow:last');
   var showClass = 'isShow';
   //showUp_variables
@@ -10,7 +10,7 @@ jQuery(function(){
   var forShow2 = jQuery('.forShow2');
   var forShow3 = jQuery('.forShow3');
   var forShow4 = jQuery('.forShow4');
-  var forShows = [forShow1,forShow2,forShow3,forShow4];
+  var forShows = [forShow1, forShow2, forShow3, forShow4];
   //textillate_variables
   var heading = jQuery('.heading');
   //commentAnim_variables
@@ -22,77 +22,78 @@ jQuery(function(){
   var contentsHeight = jQuery(document).height();
 
   ////scrollEvents////
-  jQuery(window).on('scroll',_.throttle(function(){
+  jQuery(window).on('scroll', _.throttle(function () {
 
     //common_variables
     var windowHeight = jQuery(this).height();
     var scroll = jQuery(this).scrollTop();
 
     //navHeaderShow_func
-    if(scroll > 700){
-      logoMove.css('display','inline-block');
+    if (scroll > 700) {
+      logoMove.css('display', 'inline-block');
       cloneNav.addClass(showClass);
     } else {
       cloneNav.removeClass(showClass);
     }
     //showUp_func
-    for (var j = 0; len = forShows.length, j < len ; j++) {
-      forShows[j].each(function(i){
+    for (var j = 0; len = forShows.length, j < len; j++) {
+      forShows[j].each(function (i) {
         var forShowPoint = jQuery(this).offset().top - 30;
         var delayTime = 90;
-        if (scroll > forShowPoint - windowHeight){
-          jQuery(this).delay(i*delayTime).queue(function(){
+        if (scroll > forShowPoint - windowHeight) {
+          jQuery(this).delay(i * delayTime).queue(function () {
             jQuery(this).addClass('showUp');
           });
         }
       });
     }
     //textillate_func
-    heading.each(function(){
+    heading.each(function () {
       var headingPoint = jQuery(this).offset().top;
-      if (scroll > headingPoint - windowHeight){
-        jQuery(this).css('opacity',1).textillate({
+      if (scroll > headingPoint - windowHeight) {
+        jQuery(this).css('opacity', 1).textillate({
           in: {
             effect: 'fadeInUp',
             shuffle: true,
             delay: 20,
             delayScale: 3,
-          }});
+          }
+        });
       }
     });
     //commentAnim_func
-    if(commentAnim.length){
+    if (commentAnim.length) {
       var myCommentPoint = commentAnim.offset().top;
-      if (scroll > myCommentPoint - windowHeight){
-        commentAnim.css('opacity',1).addClass('animated zoomInDown');
+      if (scroll > myCommentPoint - windowHeight) {
+        commentAnim.css('opacity', 1).addClass('animated zoomInDown');
       }
     }
     //rollBack_func
-    if(scroll > 700){
+    if (scroll > 700) {
       rollBack.fadeIn(500);
     } else {
       rollBack.fadeOut(500);
     }
     //helloMogi_func
-    if(contentsHeight - (scroll + windowHeight) < 100){
-      mogiFooter.show(function(){
+    if (contentsHeight - (scroll + windowHeight) < 100) {
+      mogiFooter.show(function () {
         jQuery(this).addClass('helloMogi');
       });
     } else {
-      if(mogiFooter.hasClass('helloMogi')){
-        mogiFooter.fadeOut(200,function(){
+      if (mogiFooter.hasClass('helloMogi')) {
+        mogiFooter.fadeOut(200, function () {
           jQuery(this).removeClass('helloMogi');
         });
       }
     }
 
-  },150)); 
+  }, 150));
   ////scrollEvents////
 
   ////optional_func////
   //rollBackClicked
-  rollBack.click(function(){
-    jQuery('body,html').animate({scrollTop : 0},700,'easeInOutQuart');
+  rollBack.click(function () {
+    jQuery('body,html').animate({ scrollTop: 0 }, 700, 'easeInOutQuart');
   });
   ////optional_func////
 
@@ -100,7 +101,7 @@ jQuery(function(){
 ////////////scrollEvents_end////////////
 
 ////////////spNavHeader////////////
-jQuery(function(){
+jQuery(function () {
   var hambBtns = jQuery('#hambBtn');
   var hambBtn1 = jQuery('.hambBtn1');
   var hambBtn2 = jQuery('.hambBtn2');
@@ -110,50 +111,56 @@ jQuery(function(){
   var spNavLink = jQuery('#spNavHeader li a');
   var delayTime = 80;
   var fadeTime = 200;
-  hambBtns.click(function(){
-    spNav.fadeToggle(fadeTime);
-    hambBtn1.toggleClass('hambMove1');
-    hambBtn2.toggleClass('hambMove2');
-    hambBtn3.toggleClass('hambMove3');
-    if(spNavLists.css('display') == 'list-item'){
+  hambBtns.click(function () {
+    toggleNav();
+    if (spNavLists.css('display') == 'list-item') {
       spNavLists.fadeOut(fadeTime);
     } else {
-      spNavLists.each(function(i){
-        jQuery(this).delay(i*delayTime).fadeIn(fadeTime);
+      spNavLists.each(function (i) {
+        jQuery(this).delay(i * delayTime).fadeIn(fadeTime);
       });
     }
   });
-  spNavLink.click(function(){
-    spNav.fadeToggle(fadeTime);
+  spNavLink.click(function () {
+    toggleNav();
+    spNavLists.hide();
+  });
+  function toggleNav() {
+    if (!spNav.hasClass('is-show')) {
+      spNav.fadeToggle(fadeTime).toggleClass('is-show');
+    } else {
+      spNav.fadeToggle(fadeTime, function () {
+        jQuery(this).toggleClass('is-show');
+      })
+    }
     hambBtn1.toggleClass('hambMove1');
     hambBtn2.toggleClass('hambMove2');
     hambBtn3.toggleClass('hambMove3');
-    spNavLists.hide();
-  });
+  };
 });
 ////////////spNavHeader////////////
 //↑can not replace↓//
 ////////////smoothScroll////////////
-jQuery(function() {
-  jQuery('.navHeader a[href^="#"]').click(function(){
+jQuery(function () {
+  jQuery('.navHeader a[href^="#"]').click(function () {
     var href = jQuery(this).attr('href');
     var target = jQuery(href == '#' || href === '' ? 'html' : href);
     var position = target.offset().top;
-    if(href === '#contact'){
-      jQuery('html,body').animate({scrollTop : position}, 700,'easeInOutQuart');
+    if (href === '#contact') {
+      jQuery('html,body').animate({ scrollTop: position }, 700, 'easeInOutQuart');
     } else {
-      jQuery('html,body').animate({scrollTop : position - 50}, 700,'easeInOutQuart');
+      jQuery('html,body').animate({ scrollTop: position - 50 }, 700, 'easeInOutQuart');
     }
     return false;
   });
-  jQuery('#spNavHeader a[href^="#"]').click(function(){
+  jQuery('#spNavHeader a[href^="#"]').click(function () {
     var href = jQuery(this).attr('href');
     var target = jQuery(href == '#' || href === '' ? 'html' : href);
     var position = target.offset().top;
-    if(href === '#contact'){
-      jQuery('html,body').animate({scrollTop : position + 100}, 700,'easeInOutQuart');
+    if (href === '#contact') {
+      jQuery('html,body').animate({ scrollTop: position + 100 }, 700, 'easeInOutQuart');
     } else {
-      jQuery('html,body').animate({scrollTop : position + 50}, 700,'easeInOutQuart');
+      jQuery('html,body').animate({ scrollTop: position + 50 }, 700, 'easeInOutQuart');
     }
     return false;
   });
@@ -161,22 +168,22 @@ jQuery(function() {
 ////////////smoothScroll////////////
 
 ////////////otherpage to frontpage////////////
-jQuery(window).on('load', function() {
+jQuery(window).on('load', function () {
   var fixPx = -50;
   var url = jQuery(location).attr('href');
-  if(url.indexOf("?id=") != -1){
+  if (url.indexOf("?id=") != -1) {
     var idName = url.split("?id=");
     var target = jQuery('#' + idName[idName.length - 1]);
-    if(target.length){
+    if (target.length) {
       var pos = target.offset().top + fixPx;
       if ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0) {
-        if( target.prop('id') == "contact" ){
+        if (target.prop('id') == "contact") {
           jQuery("html, body").scrollTop(pos + 150);
         } else {
           jQuery("html, body").scrollTop(pos + 100);
         }
-      }else{
-        if( target.prop('id') == "contact" ){
+      } else {
+        if (target.prop('id') == "contact") {
           jQuery("html, body").scrollTop(pos + 50);
         } else {
           jQuery("html, body").scrollTop(pos);
@@ -189,21 +196,21 @@ jQuery(window).on('load', function() {
 
 ////////////modal////////////
 jQuery(function () {
-  jQuery('.modalOpen').click(function(){
-    if(jQuery(this).parents('#spNavHeader')[0]){
+  jQuery('.modalOpen').click(function () {
+    if (jQuery(this).parents('#spNavHeader')[0]) {
       jQuery('#modalArea').show();
     } else {
       jQuery('#modalArea').fadeIn();
     }
   });
-  jQuery('#modalClose,#modalOuter').click(function(){
+  jQuery('#modalClose,#modalOuter').click(function () {
     jQuery('#modalArea').fadeOut();
   });
 });
 ////////////modal////////////
 
 ////////////scroll control for fuckin'IE11////////////
-if(navigator.userAgent.match(/MSIE 10/i) || navigator.userAgent.match(/Trident\/7\./) || navigator.userAgent.match(/Edge\/12\./)) {
+if (navigator.userAgent.match(/MSIE 10/i) || navigator.userAgent.match(/Trident\/7\./) || navigator.userAgent.match(/Edge\/12\./)) {
   jQuery('body').on("mousewheel", function () {
     event.preventDefault();
     var wd = event.wheelDelta;
