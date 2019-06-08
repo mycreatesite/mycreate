153c9f06a88e9caf47f1185388cb10b9fb0f812c
ma-ya's create <hello.myscreate@gmail.com>
2019-06-09 01:56:39 +0900

    changed autoprefixer browserlist

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,16 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer({
+      cascade: false,
+      grid: true
+    }))
     .pipe(gulp.dest('./css'));
 });
 
