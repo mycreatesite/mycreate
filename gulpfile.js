c522a4e5a5ce4d35f3da7b322611e94826e968a6
ma-ya's create <hello.myscreate@gmail.com>
2020-04-25 16:03:52 +0900

    gulp sass glob added

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(autoprefixer({
-      cascade: false,
-      grid: true
-    }))
-    .pipe(gulp.dest('./css'));
+	return gulp.src('./sass/**/*.scss')
+		.pipe(sassGlob())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sass({ outputStyle: 'compressed' }))
+		.pipe(autoprefixer({
+			cascade: false,
+			grid: true
+		}))
+		.pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+	gulp.watch('./sass/**/*.scss', ['sass']);
 });
