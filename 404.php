d6a34b1c06ee6199700cf2c9bba8e12aabf93810
ma-ya's create <hello.myscreate@gmail.com>
2020-06-28 03:16:14 +0900

    fix: 404

--- 404.php
+++ 404.php
@@ -1,11 +1,11 @@
 
     <?php get_header();?>
-
-    <div class="container">
-      <p><i class="far fa-eye-slash err404"></i></p>
-      <p class="err404">404 error</p>
-      <p>お探しのページは見つかりませんでした。</p>
-      <p>許してください。</p>
-    </div>
-
+	<main class="main">
+		<div class="container">
+		<p><i class="far fa-eye-slash err404"></i></p>
+		<p class="err404">404 error</p>
+		<p>お探しのページは見つかりませんでした。</p>
+		<p>許してください。</p>
+		</div>
+	</main>
     <?php get_footer();?>
